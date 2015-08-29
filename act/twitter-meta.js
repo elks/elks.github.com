f9094347633032e1fc0c8de31c08febb50a1cf55
Erik <git@soderstrome.com>
2015-08-29 19:28:26 -0400

    Setting file/directory structure and removing unwanted files.

--- act/twitter-meta.js
+++
@@ -1,4 +0,0 @@
-window.onload = twittermeta;
-function twittermeta() {
-document.getElementsByTagName('head')[0].appendChild("<meta name='twitter:site' content='@ACTforAmerica'><meta name='twitter:card' content-'summary'");
-}
